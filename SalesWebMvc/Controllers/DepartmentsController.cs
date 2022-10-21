228de95e0f73db3393b009af46a1507a54ea9cad
Fabio <fabio@ifabio.com.br>
2022-10-21 11:51:30 -0300

    Primeiro MVC - Departamentos

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SalesWebMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Deparment> list = new List<Deparment>();
+            list.Add(new Deparment { Id = 1, Name = "Eletronics" });
+            list.Add(new Deparment { Id = 2, Name = "Fashion" });
+
+            return View();
+        }
+    }
+}
